3e94b2702bd0aeea2c669178715b4f15b1981940
iczky <fiqra22@gmail.com>
2024-05-08 13:33:40 +0700

    feature: adding Testimonials section

--- app/components/Testimonials/TestimonialReview.tsx
+++ app/components/Testimonials/TestimonialReview.tsx
@@ -9,9 +9,9 @@ const TestimonialReview: React.FC<testimoniType> = ({
 }) => {
   return (
     <div className="flex flex-col gap-16">
-      <p>{desc}</p>
+      <p className="text-2xl font-medium">{desc}</p>
       <div className="flex gap-3">
-        <Image src={imgProfile} alt="Picture" />
+        <Image src={imgProfile} alt="Picture" width={64} height={64} />
         <div className="flex flex-col gap-2 justify-center text-lg">
           <h2>{name}</h2>
           <p>{role}</p>
